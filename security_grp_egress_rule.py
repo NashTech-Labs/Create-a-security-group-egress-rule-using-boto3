7a932511ddce98029e824bf860d606237fd5db01
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 00:33:49 +0530

    Update in code

--- security_grp_egress_rule.py
+++ security_grp_egress_rule.py
@@ -5,19 +5,19 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-AWS_REGION = input("Please enter the AWS_REGION")
-logger = logging.getLogger()
+REGION = input("Please enter the REGION")
+logger_for = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+client = boto3.client("ec2", region_name=REGION)
 
 
-def egress_rule(security_group_id):
+def egress_rule(security_grp_id):
 
     try:
-        response = vpc_client.authorize_security_group_egress(
-            GroupId=security_group_id,
+        res = client.authorize_security_group_egress(
+            GroupId=security_grp_id,
             IpPermissions=[{
                 'IpProtocol': 'tcp',
                 'FromPort': 80,
@@ -35,15 +35,15 @@ def egress_rule(security_group_id):
             }])
 
     except ClientError:
-        logger.exception('It can not be create egress security group rule.')
+        logger_for.exception('It can not be create egress security group rule.')
         raise
     else:
-        return response
+        return res
 
 
 if __name__ == '__main__':
-    SECURITY_GROUP_ID = input("Enter the security group ID")
-    logger.info(f'Please wait, We are checking a security group egress rule...')
-    rule = egress_rule(SECURITY_GROUP_ID)
-    logger.info(
+    SECURITY_GRP_ID = input("Enter the security group ID")
+    logger_for.info(f'Please wait, We are checking a security group egress rule...')
+    rule = egress_rule(SECURITY_GRP_ID)
+    logger_for.info(
         f'Wow!! Your Security group egress rule has been created : \n{json.dumps(rule, indent=4)}')
